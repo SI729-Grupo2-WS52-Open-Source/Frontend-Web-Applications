54bf196742821c6b688807ce309ced9a831e2de6
Nirvana Garcia <nirvagarciav@gmail.com>
2023-09-27 19:33:45 -0500

    feat: Agregación de la v2.0 del frontend

---
+++ v2.0/src/app/components/cart-page/cart-page.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {cart, priceSummary} from "../../models/data-model";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.css']
+})
+export class CartPageComponent {
+  cartData: cart[] | undefined;
+  priceSummary: priceSummary = {
+    price: 0,
+    discount: 0,
+    tax: 0,
+    delivery: 0,
+    total: 0
+  }
+  constructor(private product: ProductService, private router: Router) {
+  }
+
+  ngOnInit(){
+    this.product.currentCart().subscribe((result) => {
+      this.cartData = result;
+      let price = 0;
+      result.forEach((item) => {
+        if (item.quantity){
+          price = price+(+item.price * +item.quantity);
+        }
+      });
+      this.priceSummary.price = price;
+      this.priceSummary.discount = price/10;
+      this.priceSummary.tax = price/10;
+      this.priceSummary.delivery = 100;
+      this.priceSummary.total = price + (price/10) + 100 - (price/10);
+      console.warn(this.priceSummary);
+    });
+  }
+
+  checkout(){
+    this.router.navigate(['/checkout']);
+  }
+}
